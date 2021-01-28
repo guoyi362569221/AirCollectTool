5f211390f86f4fac95008d6197162769cf88f415
郭大力 <362569221@qq.com>
2021-01-28 20:33:17 +0800

    guoyi

--- MonitorDataSys/MonitorDataSys/UtilTool/IJobTool/LoadHourJob.cs
+++ MonitorDataSys/MonitorDataSys/UtilTool/IJobTool/LoadHourJob.cs
@@ -25,7 +25,7 @@ namespace MonitorDataSys.UtilTool.IJobTool
                     //1.绑定需要执行的操作方法
                     //var act = new Action(FrDataCollect.frDataCollect.collectHourDataTool);
                     //act.BeginInvoke(ar => act.EndInvoke(ar), null);  //参数null可以作为回调函数的返回参数
-                    //await FrDataCollect.frDataCollect.collectHourDataTool();
+                    await FrDataCollect.frDataCollect.collectHourDataTool();
                 }
                 catch (Exception e)
                 {
